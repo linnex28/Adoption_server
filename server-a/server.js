d1fac2bcf89f0cd06258bbe4b2ac0f5133c28b69
Linnea Rehu <edenwalx@gmail.com>
2025-12-15 19:11:31 +0200

    Installed CORS to Server A to fix issue with frontend connection

--- server-a/server.js
+++ server-a/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const fs = require("fs");
+const cors = require('cors');
 const server = express();
 const PORT = 3000;
 server.use(express.json());
+server.use(cors());
 
 const animalData = require('./data.json'); // (animal data created with the help of AI)
 
