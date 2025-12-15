4be12f478558d6fd6fe0e532ed5f53535ccee74e
Linnea Rehu <edenwalx@gmail.com>
2025-12-15 23:05:12 +0200

    Got application form working by fixing server2.js

--- server-b/server2.js
+++ server-b/server2.js
@@ -2,20 +2,27 @@ const express = require('express');
 const fs = require("fs");
 const server = express();
 const PORT = 8000;
-server.use(express.json());
 const cors = require('cors');
+const path = require('path');
+
+const DATA_FILE = path.join(__dirname, 'adoptiondata.json');
+
+server.use(express.json());
 server.use(cors());
 
-const adoptionData = require('./adoptiondata.json');
-
 // read adoptiondata and parse it
-async function readAdoptionData() {
+function readAdoptionData() {
     try {
-        // "await" makes server wait until file reading is done
-        const dataString = await fs.readFile(adoptionData, 'utf8');
+        const dataString = fs.readFileSync(DATA_FILE, 'utf8');
         // convert string to js object
-        const data = JSON.parse(dataString)
-        return data;
+        const parsedData = JSON.parse(dataString);
+
+        if (Array.isArray(parsedData)) {
+            return parsedData;
+        } else {
+            console.warn("adoptiondata.json doesn't contain an array");
+            return []; // return empty array
+        }
     } catch (error) {
         return "error reading adoption data";
     }
@@ -23,13 +30,42 @@ async function readAdoptionData() {
 
 // convert js object to string and writes it to adoptiondata.json
 // needed to save new adoptions to json file
-async function writeAdoptionData(data) {
+function writeAdoptionData(data) {
+  try {
     const jsonData = JSON.stringify(data, null, 2);
-    await fs.writeFile(adoptionData, jsonData, 'utf8');
+    fs.writeFileSync(DATA_FILE, jsonData, 'utf8');
+  } catch (error) {
+    console.error("Error writing adoption data:", error);
+    }
 }
 
+// get existing adoption applications
 server.get('/adoptions', (req, res) => {
-  res.json(adoptionData);
+    const adoptionData = readAdoptionData();
+    res.json(adoptionData);
+});
+
+server.post('/adoptions', (req, res) => {
+    const newApplication = req.body;
+    
+    // read existing adoption applications
+    const existingAdoptions = readAdoptionData();
+    
+    // create new ID
+    newApplication.applicationId = Date.now().toString();
+    newApplication.status = 'Pending';
+    
+    // add new application to table
+    existingAdoptions.push(newApplication);
+    
+    // write updated applications back to file
+    writeAdoptionData(existingAdoptions);
+
+    // send response back to frontend
+    res.status(201).json({ 
+        message: 'Hakemus vastaanotettu', 
+        applicationId: newApplication.applicationId 
+    });
 });
 
 server.listen(PORT, () => {
