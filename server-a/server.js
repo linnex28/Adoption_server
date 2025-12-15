a30e28d97db072804a820c9a4971548697dc4586
Linnea Rehu <edenwalx@gmail.com>
2025-12-15 15:54:21 +0200

    Server-a: express-server, port + first GET. Tested that server works

--- server-a/server.js
+++ server-a/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const server = express();
+const PORT = 3000;
+server.use(express.json());
 
-const animalData = require('./data.json');
+const animalData = require('./data.json'); // (animal data created with the help of AI)
+
+server.get('/animals', (req, res) => {
+  res.json(animalData);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`Try server at http://localhost:${PORT}/animals`);
+});
